2d646a8f98138ae1dcdf45bc3ec60793c8ffeaa0
Gabriel Torello <gabriel.torello@blake.com.au>
2016-03-24 00:08:51 +1100

    Add test cases

--- test/test_polite-gem.rb
+++ test/test_polite-gem.rb
@@ -6,6 +6,10 @@ class Mock
     "something"
   end
 
+  def self.another_complex_stuff
+    "other thing"
+  end
+
 end
 class TestPoliteGem < MiniTest::Test
   def test_an_object_can_be_asked_politly_to_do_something
@@ -14,5 +18,9 @@ class TestPoliteGem < MiniTest::Test
 
   def test_an_object_acept_gratitude
     assert_equal "something", Mock.new.please.do_complex_operation.thanks!
+  end
+
+  def test_an_object_acept_gratitude_at_class_instance
+    assert_equal "other thing", Mock.please.another_complex_stuff.thanks!
   end
 end
